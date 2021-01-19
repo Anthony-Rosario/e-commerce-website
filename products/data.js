47621b5b579491a91c3cf60d3bf85c23c0f2674b
Anthony Rosario <anthonymrosario225@gmail.com>
2021-01-19 14:57:26 -0800

    created data array

---
+++ products/data.js
@@ -0,0 +1,56 @@
+export const skateboards = [
+    {
+        id: 'Bennett',
+        name: 'Bennett Emotional Baggage Deck',
+        image: 'bennett-deck.webp',
+        description: 'Size: 8.125" Wheelbase: 14"',
+        category: 'skateboard',
+        price: 55,
+
+    },
+    {
+        id: 'Wimer',
+        name: 'Past-Forms',
+        image: 'zer0-past-forms.webp',
+        description: 'Size: 8.25" Wheelbase: 14.25"',
+        category: 'skateboard',
+        price: 59,
+
+    },
+    {
+        id: 'Delfino',
+        name: 'Santa Cruz Delfino Tarot Card',
+        image: 'Santa-Cruz-deck.webp',
+        description: 'Size: 8.25" Wheelbase: 14"',
+        category: 'skateboard',
+        price: 65,
+
+    },
+    {
+        id: 'Sector 9',
+        name: 'Stranded Strand',
+        image: 'STRANDED-STRAND.png',
+        description: 'Components: 8.375” Gullwing Mission Trucks, ABEC 5 Greaseball Bearings',
+        category: 'longboard',
+        price: 189,
+
+    },
+    {
+        id: 'Landyatchz',
+        name: 'Landyatchz Totem Paradise',
+        image: 'Landyatchz-Totem-Paradise.webp',
+        description: 'Components: Hawgs 63mm 78a wheels, Bear Space Balls ABEC 7 Bearings',
+        category: 'longboard',
+        price: 179,
+
+    },
+    {
+        id: 'DB',
+        name: 'DB Longboards Pioneer',
+        image: 'DB-Longboards-Pioneer-deck.webp',
+        description: 'Components: Cloud Ride Cruiser 69mm 78a wheels, Cloud Ride bearings',
+        category: 'longboard',
+        price: 189,
+
+    }
+];
